d155865f0d986936befc539aa3ea39890d490160
Rohan <rohans540@gmail.com>
2021-06-14 00:20:36 +0530

    Project setup and backend routes to GET products

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+
+const { getAllProducts, getProductById } = require('../controller/productControllers');
+
+//@desc GET all products from DB
+//@route GET /api/products
+//@access Public
+router.get('/', getAllProducts);
+
+//@desc GET a product from DB
+//@route GET /api/product/:id
+//@access Public
+router.get('/:id', getProductById);
+
+
+module.exports = router;
