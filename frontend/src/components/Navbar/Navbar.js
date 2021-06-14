4d80a1f9c135cc49bac9afe2c1a38cbe86f3eea8
Rohan <rohans540@gmail.com>
2021-06-15 00:54:21 +0530

    Navbar and Navbar Styling completed

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -11,10 +11,12 @@ const Navbar = () => {
 
             <ul className="navbar__links">
                 <li>
-                    <Link to='/cart'>
+                    <Link to='/cart' className="cart__link">
                         <i className="fas fa-shopping-cart"></i>
-                        Cart
-                        <span className="cartlogo__badge">0</span>
+                        <span>
+                            Cart
+                            <span className="cartlogo__badge">0</span>
+                        </span>
                     </Link>
                 </li>
 
